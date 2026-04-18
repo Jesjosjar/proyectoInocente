4d30c0c2253b56cd06c5f5c89834d74fdf60b9c4
Your Name <you@example.com>
2026-04-17 19:59:05 -0600

    Actualizaci├│n autom├ítica

--- app.js
+++ app.js
@@ -1,91 +1,28 @@
-let tareas = JSON.parse(localStorage.getItem('tareas')) || []
-let historialAcciones = []
-
-function crear(tarea) {
-    tareas.push(tarea)
-    localStorage.setItem('tareas', JSON.stringify(tareas))
-    prepararDeshacerCrear(tarea)
-}
-
-function prepararDeshacerCrear(tarea) {
-    let undoObject = {
-        fun: deshacerCrear,
-        data: [tareas.length, tarea] 
-    }
-    historialAcciones.push(undoObject)
-}
-
-function deshacerCrear(data) {
-    tareas.pop()
-    localStorage.setItem('tareas', JSON.stringify(tareas))
-}
-
-
-function eliminar(idTarea) {
-    prepararDeshacerEliminar(idTarea)
-    tareas.splice(idTarea, 1)
-    localStorage.setItem('tareas', JSON.stringify(tareas))
-}
-
-
-function prepararDeshacerEliminar(idTarea) {
-    let undoObject = {
-        fun: deshacerEliminar,
-        data: [idTarea, tareas[idTarea]]
-    }
-    historialAcciones.push(undoObject)
-}
-
-function deshacerEliminar(data) {
-    tareas.splice(data[0], 0, data[1])
-    localStorage.setItem('tareas', JSON.stringify(tareas))
-}
-
-function deshacer() {
-    let undoObject = historialAcciones[historialAcciones.length - 1]
-    historialAcciones.pop()
-    undoObject.fun(undoObject.data)
-    console.log(historialAcciones[historialAcciones.length - 1].data[1])
-}
-
-//parte visual
-window.onload = () => {
-    let ul_tareas = document.getElementById('ul_tareas')
-    let tareas = JSON.parse(localStorage.getItem('tareas')) || []
-    ul_tareas.innerHTML = ''
-    for (let i in tareas) {
-        let li = document.createElement('li')
-        li.textContent = tareas[i]
-        let button = document.createElement('button')
-        button.textContent = 'Eliminar'
-        button.onclick = () => btnEliminar(i)
-        li.appendChild(button)
-        ul_tareas.appendChild(li)
-
-    }
-}
-
-function btnCrear() {
-    let input_tarea = document.getElementById('input_tarea')
-    let ul_tareas = document.getElementById('ul_tareas')
-    let li = document.createElement('li')
-    crear(input_tarea.value)
-
-    li.textContent = input_tarea.value
-    ul_tareas.appendChild(li)
-    input_tarea.value = ''
-
-    window.onload()
-}
-
-function btnDeshacer() {
-    deshacer()
+var datos = JSON.parse(localStorage.getItem("datos")) || []
+window.onload = ()=>{
+    let lista = document.getElementById("lista")
     
-    window.onload()
+    for(let i = 0; i < datos.length; i++){
+        let li = document.createElement('li')
+        li.textContent = datos[i]
+        lista.appendChild(li)
+    }
 }
+function guardar() {
+    let input = document.getElementById('input').value
+    let lista = document.getElementById('lista')
+    let li = document.createElement('li')
+    li.textContent = input
+    lista.appendChild(li)
+    datos.push(input)
+    localStorage.setItem("datos", JSON.stringify(datos))
+}
+function deshacer() {
+    let lista = document.getElementById('lista')
+    datos.pop()
+    localStorage.setItem("datos", JSON.stringify(datos))
+    lista.removeChild(lista.lastChild)
+    
+    console.log(datos[datos.length - 1] || "No hay datos guardados")
 
-function btnEliminar(i){
-    eliminar(i)
-
-    window.onload()
 }
